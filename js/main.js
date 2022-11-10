cf9f19b88a1fb315e12ca290ca43ddedd24304d9
KellerMarika <marikakeller92@gmail.com>
2022-11-10 14:46:21 +0100

    to do list

--- js/main.js
+++ js/main.js
@@ -132,3 +132,45 @@ play_Btn.addEventListener("click", function () {
 });
 
 
+/* 
+Consegna
+Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco (attenzione: non bisogna copiare tutta la cartella dell’esercizio ma solo l’index.html, e le cartelle js/ css/ con i relativi script e fogli di stile, per evitare problemi con l’inizializzazione di git).
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+In seguito l’utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+BONUS:
+Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
+- difficoltà 1 ⇒ 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
+- difficoltà 2 ⇒ 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
+- difficoltà 3 ⇒ 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
+SUPER-BONUS:
+Quando si clicca su una bomba e finisce la partita, evitare che si possa cliccare su altre celle.
+Quando si clicca su una bomba e finisce la partita, il software scopre tutte le bombe nascoste.
+ */
+
+/* correzioni */
+//sostituire  classe nominale con dataset
+//fix possibile errore caricamento con d-none alla creazione della griglia e rimozione alla fine
+//rimuovo numerazione interna
+//correzione allinterno della funzione genera griglia, cell.addevent listner scatena la funzione controllo se bomba
+
+
+
+/* cosa devo fare? */
+//funzione generica genera tot numeri casuali e li racchiude in un array
+//f generate random number array (minNumber, maxNumber, resultArray.lenghtNumber)
+//dentro al play button che genera la griglia in base al valore della select raccolto:
+///assegno il risultato della funzione all'array globale undefined bombe bombs_list = generate random number array (1, totalCells_Number, 16);
+//funzione generica find element (array da controllare(bombs))
+//che scateno al clic sulle singole celle cell.add event listner (click, find element) se si aggiungi classe magica bomba
+
+//aggiungere un contatore esterno che si aggiorna nella funzine click cell solo se non è bomb e che a fine partita comunichi attraverso funzioni di calcolo il risultato al giocatore (in base a quante caselle fatte e quante mancanti valutazione soggettiva. se le ha fatte tutte mega premio);
+
+
+//__________________
+//disattivare il click sulle altre celle se se ne becca una con la bomba (return? oppure rimozione di add event listner da tutte le altre(comprese quelle già fatte))
+//scopire tutte le bombe nascoste se perdi
+
+
+
